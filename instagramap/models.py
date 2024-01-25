7da3563a52028943af3897b723574a53643136eb
muhayimanatheo <muhayitheo23@gmail.com>
2024-01-25 23:04:51 +0200

    ready for getting data in table

--- instagramap/models.py
+++ instagramap/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class myProfile(models.Model):
+    username = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=100)
+    button = models.CharField(max_length=100)
+    bio = models.TextField(max_length=100)
+
+    def __str__(self):
+        return self.username
--- instagramap/views.py
+++ instagramap/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
+def index(request):
+    selecctData = myProfile.objects.all()
+    return render(request,'index.html', {'data': selecctData})
